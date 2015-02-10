962ae832b35c1e11d3629395d38c1ec12df1ed82
wzsmith1 <wzsmith1@yahoo.com>
2015-02-09 21:00:06 -0500

    angular changes

--- MvcApplication/MvcApplication/Scripts/Home/index2.js
+++ MvcApplication/MvcApplication/Scripts/Home/index2.js
@@ -3,6 +3,8 @@ function foodController($scope, $http)
 {
     $scope.sample1 = "Sample 1";
     $scope.sample2 = "Sample 2";
+    $scope.isBusy = true;
+    $scope.foodInput = {};
 
     $scope.dataSingle;
     $http.get("/api/Food/GetFood/10")
@@ -27,7 +29,17 @@ function foodController($scope, $http)
             alert("failure")
         });
 
-
+    $scope.save = function () {
+        $http.get("/api/Food/GetFoods/" + $scope.foodInput.Value)
+            .then(function (result) {
+                //Successful
+                angular.copy(result.data, $scope.dataMultiple);
+            },
+        function () {
+            //Failure
+            alert("failure")
+        });
+    }
 
     //$scope.data = [
     //    {
