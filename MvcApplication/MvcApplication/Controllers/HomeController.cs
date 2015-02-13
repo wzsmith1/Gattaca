d8296b8558ec6d92244a55881b51401de156f0ce
wzsmith1 <wzsmith1@yahoo.com>
2015-02-13 16:47:25 -0500

    final final gitignore commit

--- MvcApplication/MvcApplication/Controllers/HomeController.cs
+++ MvcApplication/MvcApplication/Controllers/HomeController.cs
@@ -39,7 +39,6 @@ namespace MvcApplication.Controllers
 
         public ActionResult Index2()
         {
-            var x = 0;
             string searchTerm = "Cranberry";
             var foods = _repo.GetFoodsBySearchTerm(searchTerm)
                 .OrderBy(f => f.Name)
