fa8c6065a381205214254d33faa9ebf205164008
wzsmith1 <wzsmith1@yahoo.com>
2015-02-22 14:36:09 -0500

    initial typeaheads

---
+++ MvcApplication/MvcApplication/Scripts/Home/autocomplete.js
@@ -0,0 +1,89 @@
+﻿
+//autocomplete
+$(document).ready(function () {
+    $("#input2").on('input', function () {
+        val = $("#input2").val();
+        $("#output2").html(val);
+        
+        var options = {};
+        options.url = "/api/Food/GetFoods/" + val;
+        options.type = "GET";
+        options.datatype = "json";
+        options.success = function (data) {
+            jsonValue = JSON.stringify(data);
+            //alert(data[0].Name);
+            for(var i=0; i<data.length; i++)
+            {
+                $("#foodList2").append(
+                    "<option value='" + data[i].Name + "'></option>");
+            }
+        }
+        options.failure = function (data) {
+            alert("failure");
+        }
+        $.ajax(options);
+    });
+});
+
+
+
+
+//typeahead
+$(document).ready(function () {
+var substringMatcher = function (strs) {
+    return function findMatches(q, cb) {
+        var matches, substrRegex;
+
+        // an array that will be populated with substring matches
+        matches = [];
+
+        // regex used to determine if a string contains the substring `q`
+        substrRegex = new RegExp(q, 'i');
+
+        // iterate through the pool of strings and for any string that
+        // contains the substring `q`, add it to the `matches` array
+        $.each(strs, function (i, str) {
+            if (substrRegex.test(str)) {
+                // the typeahead jQuery plugin expects suggestions to a
+                // JavaScript object, refer to typeahead docs for more info
+                matches.push({ value: str });
+            }
+        });
+
+        cb(matches);
+    };
+};
+
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+
+$('#typeahead .typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+},
+{
+    name: 'states',
+    displayKey: 'value',
+    source: substringMatcher(states)
+});
+});
+
+
+
+
+
+
+
+
+
+
+
